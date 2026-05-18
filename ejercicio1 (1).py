15bfb90fcd42e6d1cae73f2d3c7afe20766a0056
pobleteirmelin0-beep <pobleteirmelin0@gmail.com>
2026-05-18 00:33:03 -0400

    Add files via upload

---
+++ ejercicio1 (1).py
@@ -0,0 +1,30 @@
+print("💖 hola mi querido 💖")
+
+print("1. Carta")
+print("2. cuanto te amo")
+print("3. sorpresa ✨")
+
+opcion = input("Elige una opción: ")
+
+if opcion == "1":
+    print("mi querido")
+    print("Si existen otras vidas, otras muertes y otros universos, espero encontrarte en   cada uno de ellos.Porque siento que incluso el fin del tiempo sería incapaz de acabar con lo que siento por ti.aun cuando mi cuerpo desaparezca y solo queden cenizas de mí, sé que incluso ellas  seguirían amándote, porque no creo poder dejar de enamorarme de cada parte de ti, de cada lunar, de cada detalle y de esos ojitos suyos en los que podría perderme durante horas sin sentir que es   suficiente, porque no existe universo en el que mi corazón no vuelva a elegirlo a usted, ni    versión de mí que no termine enamorándose nuevamente de usted, porque sin importar cuándo, dónde o en qué vida sea, mi corazón siempre va a encontrar el camino de regreso a usted, y  te aseguro que podría pasar una eternidad admirándolo y aun así sentir que me faltaría tiempo para seguir amándolo…” ")
+
+if opcion == "2":
+    numero = 1
+
+    while numero > 0:
+        print(numero, "💖")
+        numero = numero + 1
+
+if opcion == "3":
+    print ("sorpresa)")
+    print("Buscando al amor de mi vida...")
+
+    print("█░░░░░░░░░ 10%")
+    print("███░░░░░░░ 30%")
+    print("█████░░░░░ 50%")
+    print("███████░░░ 70%")
+    print("██████████ 100%")
+
+    print("✔ Encontrado: Usted 💖")
